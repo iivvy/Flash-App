d1300ee52314c5f064e3e56e95cdc40cb8ccf73f
Nixi <72278701+iivvy@users.noreply.github.com>
2022-09-19 19:56:03 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:chat_app/screens/chat_screen.dart';
+import 'package:chat_app/screens/login_screen.dart';
+import 'package:chat_app/screens/register_screen.dart';
+import 'package:chat_app/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute:'/' ,
+      routes: {
+        '/':(context) =>WelcomeScreen (),
+        'login':(context)=> LoginScreen(),
+        'register':(context)=>RegistrationScreen(),
+        'chat':(context)=>ChatScreen(),
+      },
+    );
+  }
+}
